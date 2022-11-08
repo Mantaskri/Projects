c9c96da7e11e73ec8ec3429d2c6acdcc819de47d
MantasKri <mantuoske@hotmail.com>
2022-11-08 16:56:21 +0200

    Category model

--- Blog/app/Http/Controllers/HomeController.php
+++ Blog/app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $allCategories = DB::table('categories')->get();
+        $allCategories = Category::all();
 
         return view('index', ['categories' => $allCategories]);
     }
